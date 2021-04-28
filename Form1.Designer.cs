87af6c7220110a45657dc853c6884b06f36bb937
Anna <annascwo@gmail.com>
2021-04-28 18:10:24 +0100

    Alterei o form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,8 +31,8 @@ namespace marco
         {
             this.button6 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.button2 = new System.Windows.Forms.Button();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
@@ -40,7 +40,8 @@ namespace marco
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,9 +56,10 @@ namespace marco
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.radioButton3);
             this.groupBox1.Controls.Add(this.button1);
@@ -67,36 +69,34 @@ namespace marco
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Location = new System.Drawing.Point(12, 6);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(515, 229);
+            this.groupBox1.Size = new System.Drawing.Size(515, 253);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Login, my angel";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 15);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Você me ama?";
+            // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(135, 106);
+            this.radioButton1.Location = new System.Drawing.Point(137, 105);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(44, 19);
+            this.radioButton1.Size = new System.Drawing.Size(167, 19);
             this.radioButton1.TabIndex = 19;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "sim";
+            this.radioButton1.Text = "Muito, mas pq a pergunta?";
             this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(208, 106);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(155, 19);
-            this.radioButton2.TabIndex = 20;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "claro, nem pergunta dnv";
-            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(23, 176);
+            this.button2.Location = new System.Drawing.Point(23, 201);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(470, 30);
             this.button2.TabIndex = 14;
@@ -106,17 +106,17 @@ namespace marco
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(391, 106);
+            this.radioButton3.Location = new System.Drawing.Point(310, 130);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(102, 19);
+            this.radioButton3.Size = new System.Drawing.Size(79, 19);
             this.radioButton3.TabIndex = 21;
             this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "desgraçadinha";
+            this.radioButton3.Text = "Não :rage:";
             this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 140);
+            this.button1.Location = new System.Drawing.Point(23, 165);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(470, 30);
             this.button1.TabIndex = 13;
@@ -155,20 +155,33 @@ namespace marco
             this.label2.TabIndex = 18;
             this.label2.Text = "Key:";
             // 
-            // label3
+            // radioButton2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 106);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 15);
-            this.label3.TabIndex = 22;
-            this.label3.Text = "Você me ama?";
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(310, 105);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(154, 19);
+            this.radioButton2.TabIndex = 21;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Claro sua desgraçadinha";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(137, 130);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(90, 19);
+            this.radioButton4.TabIndex = 23;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Sim, é óbvio";
+            this.radioButton4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 247);
+            this.ClientSize = new System.Drawing.Size(539, 268);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button6);
             this.Name = "Form1";
@@ -183,7 +196,6 @@ namespace marco
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
@@ -192,6 +204,8 @@ namespace marco
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
