66c55bb2220055afa63ddf43975192cffbd6fd2d
Anna <annascwo@gmail.com>
2021-04-29 18:02:42 +0100

    Mais alterações no form login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,7 +42,6 @@ namespace marco
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,12 +59,9 @@ namespace marco
             this.groupBox1.Controls.Add(this.tbKey);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.panel1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 7);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 6);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox1.Size = new System.Drawing.Size(515, 292);
+            this.groupBox1.Size = new System.Drawing.Size(515, 243);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Login, my angel";
@@ -74,23 +70,21 @@ namespace marco
             // 
             this.rbSim.AutoSize = true;
             this.rbSim.BackColor = System.Drawing.SystemColors.Control;
-            this.rbSim.Location = new System.Drawing.Point(137, 156);
-            this.rbSim.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbSim.Location = new System.Drawing.Point(104, 126);
             this.rbSim.Name = "rbSim";
-            this.rbSim.Size = new System.Drawing.Size(89, 22);
+            this.rbSim.Size = new System.Drawing.Size(249, 19);
             this.rbSim.TabIndex = 23;
             this.rbSim.TabStop = true;
-            this.rbSim.Text = "Sim, é óbvio";
+            this.rbSim.Text = "Se eu amo? Eu quero casar com vc";
             this.rbSim.UseVisualStyleBackColor = false;
             // 
             // rbClaro
             // 
             this.rbClaro.AutoSize = true;
             this.rbClaro.BackColor = System.Drawing.SystemColors.Control;
-            this.rbClaro.Location = new System.Drawing.Point(310, 126);
-            this.rbClaro.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbClaro.Location = new System.Drawing.Point(307, 101);
             this.rbClaro.Name = "rbClaro";
-            this.rbClaro.Size = new System.Drawing.Size(154, 22);
+            this.rbClaro.Size = new System.Drawing.Size(186, 19);
             this.rbClaro.TabIndex = 21;
             this.rbClaro.TabStop = true;
             this.rbClaro.Text = "Claro sua desgraçadinha";
@@ -100,9 +94,9 @@ namespace marco
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(23, 127);
+            this.label3.Location = new System.Drawing.Point(7, 103);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 18);
+            this.label3.Size = new System.Drawing.Size(91, 15);
             this.label3.TabIndex = 22;
             this.label3.Text = "Você me ama?";
             // 
@@ -110,10 +104,9 @@ namespace marco
             // 
             this.rbMuito.AutoSize = true;
             this.rbMuito.BackColor = System.Drawing.SystemColors.Control;
-            this.rbMuito.Location = new System.Drawing.Point(137, 126);
-            this.rbMuito.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbMuito.Location = new System.Drawing.Point(104, 101);
             this.rbMuito.Name = "rbMuito";
-            this.rbMuito.Size = new System.Drawing.Size(166, 22);
+            this.rbMuito.Size = new System.Drawing.Size(200, 19);
             this.rbMuito.TabIndex = 19;
             this.rbMuito.TabStop = true;
             this.rbMuito.Text = "Muito, mas pq a pergunta?";
@@ -121,10 +114,9 @@ namespace marco
             // 
             // btCancel
             // 
-            this.btCancel.Location = new System.Drawing.Point(23, 241);
-            this.btCancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btCancel.Location = new System.Drawing.Point(23, 201);
             this.btCancel.Name = "btCancel";
-            this.btCancel.Size = new System.Drawing.Size(470, 36);
+            this.btCancel.Size = new System.Drawing.Size(470, 30);
             this.btCancel.TabIndex = 14;
             this.btCancel.Text = "Cancel";
             this.btCancel.UseVisualStyleBackColor = true;
@@ -134,10 +126,9 @@ namespace marco
             // 
             this.rbNao.AutoSize = true;
             this.rbNao.BackColor = System.Drawing.SystemColors.Control;
-            this.rbNao.Location = new System.Drawing.Point(310, 156);
-            this.rbNao.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbNao.Location = new System.Drawing.Point(398, 126);
             this.rbNao.Name = "rbNao";
-            this.rbNao.Size = new System.Drawing.Size(81, 22);
+            this.rbNao.Size = new System.Drawing.Size(95, 19);
             this.rbNao.TabIndex = 21;
             this.rbNao.TabStop = true;
             this.rbNao.Text = "Não :rage:";
@@ -145,10 +136,9 @@ namespace marco
             // 
             // btLogin
             // 
-            this.btLogin.Location = new System.Drawing.Point(23, 198);
-            this.btLogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btLogin.Location = new System.Drawing.Point(23, 165);
             this.btLogin.Name = "btLogin";
-            this.btLogin.Size = new System.Drawing.Size(470, 36);
+            this.btLogin.Size = new System.Drawing.Size(470, 30);
             this.btLogin.TabIndex = 13;
             this.btLogin.Text = "Login";
             this.btLogin.UseVisualStyleBackColor = true;
@@ -156,27 +146,25 @@ namespace marco
             // 
             // tbNome
             // 
-            this.tbNome.Location = new System.Drawing.Point(72, 43);
-            this.tbNome.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbNome.Location = new System.Drawing.Point(56, 32);
             this.tbNome.Name = "tbNome";
-            this.tbNome.Size = new System.Drawing.Size(421, 23);
+            this.tbNome.Size = new System.Drawing.Size(437, 21);
             this.tbNome.TabIndex = 15;
             // 
             // tbKey
             // 
-            this.tbKey.Location = new System.Drawing.Point(72, 83);
-            this.tbKey.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbKey.Location = new System.Drawing.Point(56, 65);
             this.tbKey.Name = "tbKey";
-            this.tbKey.Size = new System.Drawing.Size(421, 23);
+            this.tbKey.Size = new System.Drawing.Size(437, 21);
             this.tbKey.TabIndex = 16;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(23, 47);
+            this.label1.Location = new System.Drawing.Point(7, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 18);
+            this.label1.Size = new System.Drawing.Size(42, 15);
             this.label1.TabIndex = 17;
             this.label1.Text = "Name:";
             // 
@@ -184,9 +172,9 @@ namespace marco
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(23, 86);
+            this.label2.Location = new System.Drawing.Point(7, 68);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 18);
+            this.label2.Size = new System.Drawing.Size(35, 15);
             this.label2.TabIndex = 18;
             this.label2.Text = "Key:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -195,33 +183,26 @@ namespace marco
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Location = new System.Drawing.Point(358, 303);
+            this.label4.Location = new System.Drawing.Point(310, 252);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(169, 18);
+            this.label4.Size = new System.Drawing.Size(217, 15);
             this.label4.TabIndex = 24;
             this.label4.Text = "Feito por: Nana, sua garotinha";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Control;
-            this.panel1.Location = new System.Drawing.Point(0, 6);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(515, 287);
-            this.panel1.TabIndex = 24;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DimGray;
-            this.BackgroundImage = global::marco.Properties.Resources._11;
-            this.ClientSize = new System.Drawing.Size(539, 325);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(539, 276);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.groupBox1);
-            this.Font = new System.Drawing.Font("Sitka Subheading", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "We are hated, but I feel loved with you";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -244,7 +225,6 @@ namespace marco
         private System.Windows.Forms.RadioButton rbSim;
         private System.Windows.Forms.RadioButton rbClaro;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- love.Designer.cs
+++ love.Designer.cs
@@ -30,46 +30,38 @@ namespace marco
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.tbNumero = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.tbNumero);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(689, 223);
+            this.groupBox1.Size = new System.Drawing.Size(581, 295);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Hmmm ainda nao sei oq por";
-            // 
-            // tbNumero
-            // 
-            this.tbNumero.Location = new System.Drawing.Point(6, 49);
-            this.tbNumero.Name = "tbNumero";
-            this.tbNumero.Size = new System.Drawing.Size(572, 23);
-            this.tbNumero.TabIndex = 1;
+            this.groupBox1.Text = "Memórias";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Location = new System.Drawing.Point(31, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(166, 15);
+            this.label1.Size = new System.Drawing.Size(504, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Digita HMMMM DIGITA ALGO";
+            this.label1.Text = "Se você conseguiu entrar aqui, significa que você é o Marco, o que também signifi" +
+    "ca que você";
             // 
             // love
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(713, 542);
+            this.ClientSize = new System.Drawing.Size(605, 545);
             this.Controls.Add(this.groupBox1);
             this.Name = "love";
-            this.Text = "love";
+            this.Text = "Entre, sente-se e relaxe...";
             this.Load += new System.EventHandler(this.love_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -82,6 +74,5 @@ namespace marco
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox tbAlgumaCoisa;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox tbNumero;
     }
 }
