e0556dbb3dbde4c30aa553c27961b28526616909
Anna <annascwo@gmail.com>
2021-05-21 11:37:28 +0100

    Added labels to simulate the progress bar

--- ProgressBar.Designer.cs
+++ ProgressBar.Designer.cs
@@ -29,20 +29,57 @@ namespace amo6166
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbLoading = new System.Windows.Forms.Label();
+            this.lbBar = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lbLoading
+            // 
+            this.lbLoading.AutoSize = true;
+            this.lbLoading.Font = new System.Drawing.Font("Cascadia Code SemiLight", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbLoading.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.lbLoading.Location = new System.Drawing.Point(543, 394);
+            this.lbLoading.Name = "lbLoading";
+            this.lbLoading.Size = new System.Drawing.Size(132, 28);
+            this.lbLoading.TabIndex = 0;
+            this.lbLoading.Text = "Loading...";
+            // 
+            // lbBar
+            // 
+            this.lbBar.AutoSize = true;
+            this.lbBar.Font = new System.Drawing.Font("Cascadia Code SemiLight", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.lbBar.Location = new System.Drawing.Point(459, 355);
+            this.lbBar.Name = "lbBar";
+            this.lbBar.Size = new System.Drawing.Size(288, 28);
+            this.lbBar.TabIndex = 1;
+            this.lbBar.Text = "</////////////////////>\r\n";
             // 
             // ProgressBar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1129, 625);
+            this.ClientSize = new System.Drawing.Size(1366, 768);
+            this.ControlBox = false;
+            this.Controls.Add(this.lbBar);
+            this.Controls.Add(this.lbLoading);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Movable = false;
             this.Name = "ProgressBar";
-            this.Text = "ProgressBar";
+            this.Opacity = 0.93D;
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Black;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.Load += new System.EventHandler(this.ProgressBar_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbLoading;
+        private System.Windows.Forms.Label lbBar;
     }
 }
