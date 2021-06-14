b7aea3fa4e40f78abcd57043e842596ffb0d4601
Nana <annascwo@gmail.com>
2021-06-14 10:40:08 +0100

    Updated loading quotes

--- ProgressBar.Designer.cs
+++ ProgressBar.Designer.cs
@@ -33,19 +33,11 @@ namespace amo6166
             this.lbLoading = new System.Windows.Forms.Label();
             this.lbBar = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.fish1 = new System.Windows.Forms.Label();
-            this.fish2 = new System.Windows.Forms.Label();
-            this.fish3 = new System.Windows.Forms.Label();
-            this.fish4 = new System.Windows.Forms.Label();
-            this.fish5 = new System.Windows.Forms.Label();
-            this.fish6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbLoading
             // 
-            this.lbLoading.Font = new System.Drawing.Font("Cascadia Code SemiLight", 16.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbLoading.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lbLoading.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
             this.lbLoading.Location = new System.Drawing.Point(549, 398);
             this.lbLoading.Name = "lbLoading";
@@ -55,7 +47,7 @@ namespace amo6166
             // 
             // lbBar
             // 
-            this.lbBar.Font = new System.Drawing.Font("Cascadia Code SemiLight", 20.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lbBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
             this.lbBar.Location = new System.Drawing.Point(23, 361);
             this.lbBar.Name = "lbBar";
@@ -65,111 +57,12 @@ namespace amo6166
             this.lbBar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.lbBar.TextChanged += new System.EventHandler(this.lbBar_TextChanged);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Cascadia Code SemiLight", 16.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
-            this.label2.Location = new System.Drawing.Point(566, 442);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(234, 30);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "After Eating Fish";
-            this.label2.Visible = false;
-            // 
-            // label1
-            // 
-            this.label1.Font = new System.Drawing.Font("Cascadia Code SemiLight", 16.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
-            this.label1.Location = new System.Drawing.Point(23, 48);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1348, 266);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "B\'coz the Water May Cause the Fish\r\nto Swim and then u will Feel gulugulu gulugul" +
-    "u in ur Stomach";
-            this.label1.Visible = false;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // fish1
-            // 
-            this.fish1.Font = new System.Drawing.Font("Cascadia Code SemiLight", 20.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.fish1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
-            this.fish1.Location = new System.Drawing.Point(1294, 617);
-            this.fish1.Name = "fish1";
-            this.fish1.Size = new System.Drawing.Size(352, 131);
-            this.fish1.TabIndex = 9;
-            this.fish1.Text = "<º))))><";
-            this.fish1.Visible = false;
-            // 
-            // fish2
-            // 
-            this.fish2.Font = new System.Drawing.Font("Cascadia Code SemiLight", 20.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.fish2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
-            this.fish2.Location = new System.Drawing.Point(925, 617);
-            this.fish2.Name = "fish2";
-            this.fish2.Size = new System.Drawing.Size(352, 131);
-            this.fish2.TabIndex = 4;
-            this.fish2.Text = "<º))))><";
-            this.fish2.Visible = false;
-            // 
-            // fish3
-            // 
-            this.fish3.Font = new System.Drawing.Font("Cascadia Code SemiLight", 20.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.fish3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
-            this.fish3.Location = new System.Drawing.Point(678, 617);
-            this.fish3.Name = "fish3";
-            this.fish3.Size = new System.Drawing.Size(352, 131);
-            this.fish3.TabIndex = 5;
-            this.fish3.Text = "<º))))><";
-            this.fish3.Visible = false;
-            // 
-            // fish4
-            // 
-            this.fish4.Font = new System.Drawing.Font("Cascadia Code SemiLight", 20.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.fish4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
-            this.fish4.Location = new System.Drawing.Point(535, 617);
-            this.fish4.Name = "fish4";
-            this.fish4.Size = new System.Drawing.Size(352, 131);
-            this.fish4.TabIndex = 6;
-            this.fish4.Text = "<º))))><";
-            this.fish4.Visible = false;
-            // 
-            // fish5
-            // 
-            this.fish5.Font = new System.Drawing.Font("Cascadia Code SemiLight", 20.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.fish5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
-            this.fish5.Location = new System.Drawing.Point(224, 617);
-            this.fish5.Name = "fish5";
-            this.fish5.Size = new System.Drawing.Size(352, 131);
-            this.fish5.TabIndex = 7;
-            this.fish5.Text = "<º))))><";
-            this.fish5.Visible = false;
-            // 
-            // fish6
-            // 
-            this.fish6.Font = new System.Drawing.Font("Cascadia Code SemiLight", 20.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.fish6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
-            this.fish6.Location = new System.Drawing.Point(-59, 617);
-            this.fish6.Name = "fish6";
-            this.fish6.Size = new System.Drawing.Size(352, 131);
-            this.fish6.TabIndex = 8;
-            this.fish6.Text = "<º))))><";
-            this.fish6.Visible = false;
-            // 
             // ProgressBar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1366, 768);
             this.ControlBox = false;
-            this.Controls.Add(this.fish1);
-            this.Controls.Add(this.fish6);
-            this.Controls.Add(this.fish5);
-            this.Controls.Add(this.fish4);
-            this.Controls.Add(this.fish3);
-            this.Controls.Add(this.fish2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.lbBar);
             this.Controls.Add(this.lbLoading);
             this.MaximizeBox = false;
@@ -183,7 +76,6 @@ namespace amo6166
             this.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.Load += new System.EventHandler(this.ProgressBar_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -192,13 +84,5 @@ namespace amo6166
         private System.Windows.Forms.Label lbLoading;
         private System.Windows.Forms.Label lbBar;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label fish1;
-        private System.Windows.Forms.Label fish2;
-        private System.Windows.Forms.Label fish3;
-        private System.Windows.Forms.Label fish4;
-        private System.Windows.Forms.Label fish5;
-        private System.Windows.Forms.Label fish6;
     }
 }
